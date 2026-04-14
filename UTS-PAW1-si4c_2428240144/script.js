1b66a4cabb7bc5b71ddc5079e000a4df4888861f
AgusFirman16 <agusfirman_2428240144@mhs.mdp.ac.id>
2026-04-14 12:48:19 +0700

    Update script.js

---
+++ UTS-PAW1-si4c_2428240144/script.js
@@ -0,0 +1,38 @@
+let namaBarang = document.getElementById("namaBarang");
+let Jumlah = document.getElementById("Jumlah");
+let keterangan = document.getElementById("keterangan")
+
+function simpan(){
+    console.log(namaBarang.value);
+    console.log(Jumlah.value);
+    console.log(keterangan.value);
+
+    if(localStorage.setItem("namaBarang")===null)
+        localStorage.setItem("namaBarang", "[]");
+
+    let data = JSON.parse(localStorage.getItem("namaBarang"));
+    console.log(data);
+
+    data.push({
+        namaBarang: namaBarang.value,
+        Jumlah: Jumlah.value,
+        keterangan: keterangan.value,
+    });
+
+    console.log(data);
+    localStorage.setItem("namaBarang",JSON.stringify(data));
+
+}
+
+function tampil(){
+    let hasil = JSON.parse(localStorage.getItem("namaBarang"));
+    hasil.forEach(element => {
+        document.getElementById("list-namaBarang").innerHTML+=
+        document.getElementById("list-jumlah").innerHTML+=
+        document.getElementById("list-jumlah").innerHTML+=
+
+        
+    });
+
+}
+tampil();
